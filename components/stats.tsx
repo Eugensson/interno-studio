64b0bb0ea08b3b4bd3669928a06ac01a8c28b241
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-09-30 09:24:45 +0300

    Add animation delay in Stats component

--- components/stats.tsx
+++ components/stats.tsx
@@ -1,14 +1,21 @@
 "use client";
 
+import { useRef } from "react";
 import CountUp from "react-countup";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 
 import { statsData } from "@/lib/data";
 import { fadeIn } from "@/lib/variants";
 
 export const Stats = () => {
+  const sectionRef = useRef<HTMLElement | null>(null);
+  const isInView = useInView(sectionRef, { once: false, amount: 0.3 });
+
   return (
-    <section className="section bg-accent-secondary py-20 xl:py-37.5">
+    <section
+      ref={sectionRef}
+      className="section bg-accent-secondary py-20 xl:py-37.5"
+    >
       <div className="container">
         <motion.ul
           variants={fadeIn("up", 0.2)}
@@ -17,22 +24,26 @@ export const Stats = () => {
           viewport={{ once: false, amount: 0.2 }}
           className="grid grid-cols-1 lg:grid-cols-4 gap-12"
         >
-          {statsData.map((stat, index) => (
+          {statsData.map(({ id, label, value }, index) => (
             <li
-              key={stat.id}
+              key={id}
               className={`text-center ${
                 index !== statsData.length - 1
                   ? "xl:border-r xl:border-accent"
                   : ""
               }`}
             >
-              <CountUp
-                end={stat.value}
-                delay={0.4 + index * 0.4}
-                duration={2.5}
-                className="h1 font-primary text-accent"
-              />
-              <p>{stat.label}</p>
+              {isInView ? (
+                <CountUp
+                  end={value}
+                  duration={2.5}
+                  delay={0.4 + index * 0.4}
+                  className="h1 font-primary text-accent"
+                />
+              ) : (
+                <span className="h1 font-primary text-accent">0</span>
+              )}
+              <p className="text-lg font-medium">{label}</p>
             </li>
           ))}
         </motion.ul>
